52ac974fadb8ba32ea010822fb3be60decb2dde7
georgesebik <sebikgeorge@gmail.com>
2026-03-15 20:39:34 +0300

    FindFirstandLastPositionofElementinSortedArray

---
+++ Algorithms/tbank-interview/Medium/FindFirstandLastPositionofElementinSortedArray.cs
@@ -0,0 +1,67 @@
+namespace Algorithms.tbank_interview.Medium;
+
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+public static class FindFirstandLastPositionofElementinSortedArray
+{
+    private static int[] SearchRange(int[] nums, int target)
+    {
+        return [BinarySearch(nums, target, true), BinarySearch(nums, target)];
+    }
+
+    private static int BinarySearch(int[] nums, int target, bool findFirst = false)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+        var result = -1;
+
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                result = mid;
+                if (findFirst)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static int[] SearchRange_2Gen(int[] nums, int target)
+    {
+        int first = -1, last = -1;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                if (first == -1) first = i; // Found the first occurrence
+                last = i; // Keep updating last until the end of the target block
+            }
+        }
+
+        return [first, last];
+    }
+
+    public static void Test()
+    {
+        var result = SearchRange([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7);
+        Console.WriteLine(result);
+    }
+}
